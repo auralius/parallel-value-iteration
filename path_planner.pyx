bdd7bf9013818a87c1112575565368f77c7790b6
Auralius Manurung <auralius.manurung@formulatrix.com>
2023-06-05 03:02:31 +0700

    Get rid off some compilation warnings.

--- path_planner.pyx
+++ path_planner.pyx
@@ -24,22 +24,22 @@ ctypedef np.int_t DTYPE_t
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 
-def terrain_value_iteration(np.ndarray [DTYPE_t, ndim=2] terrain_mtx, np.ndarray [DTYPE_t, ndim=1] source, np.ndarray [DTYPE_t, ndim=1] target, np.ndarray [DTYPE_t, ndim=1] steps, int max_horizon, int append):
+def terrain_value_iteration(np.ndarray [DTYPE_t, ndim=2] terrain_mtx, np.ndarray [DTYPE_t, ndim=1] source, np.ndarray [DTYPE_t, ndim=1] target, np.ndarray [DTYPE_t, ndim=1] steps, Py_ssize_t max_horizon, DTYPE_t append):
     terrain_mtx[target[0], target[1]] = 0 # set cost at the target to 0
 
-    cdef int nX = terrain_mtx.shape[0]
-    cdef int nY = terrain_mtx.shape[1]
+    cdef DTYPE_t nX = <DTYPE_t>(terrain_mtx.shape[0])
+    cdef DTYPE_t nY = <DTYPE_t>(terrain_mtx.shape[1])
     
     # A very small number
     cdef double EPSILON = 1E-10
 
-    cdef int XMIN = 0
-    cdef int XMAX = nX - 1
-    cdef int YMIN = 0
-    cdef int YMAX = nY - 1
+    cdef DTYPE_t XMIN = 0
+    cdef DTYPE_t XMAX = nX - 1
+    cdef DTYPE_t YMIN = 0
+    cdef DTYPE_t YMAX = nY - 1
 
-    cdef int stepX = steps[0]
-    cdef int stepY = steps[1]
+    cdef DTYPE_t stepX = steps[0]
+    cdef DTYPE_t stepY = steps[1]
 
     # Create J matrix
     # row -> x
@@ -47,32 +47,32 @@ def terrain_value_iteration(np.ndarray [DTYPE_t, ndim=2] terrain_mtx, np.ndarray
     cdef np.ndarray [DTYPE_t, ndim=2] J = np.zeros((nX, nY), dtype=DTYPE)
     cdef np.ndarray [DTYPE_t, ndim=2] Jprev = np.zeros((nX, nY), dtype=DTYPE)
     
-    cdef int[:, :] J_arr = J
-    cdef int[:, :] Jprev_arr = Jprev
-    cdef int *J_ptr = &J_arr[0, 0]
-    cdef int *Jprev_ptr = &Jprev_arr[0, 0]
+    cdef DTYPE_t[:, :] J_arr = J
+    cdef DTYPE_t[:, :] Jprev_arr = Jprev
+    cdef DTYPE_t *J_ptr = &J_arr[0, 0]
+    cdef DTYPE_t *Jprev_ptr = &Jprev_arr[0, 0]
 
     # Policy matrix
     #policy = np.zeros((nX, nY))
 
-    cdef int[9] u = [0, 1, 3, 5, 7, 2, 4, 6, 8] 
-    cdef int[9] u_cost_map = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    cdef DTYPE_t[9] u = [0, 1, 3, 5, 7, 2, 4, 6, 8] 
+    cdef DTYPE_t[9] u_cost_map = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     cdef np.ndarray [DTYPE_t, ndim=2] descendantX = np.zeros((nX, nY), dtype=DTYPE)
     cdef np.ndarray [DTYPE_t, ndim=2] descendantY = np.zeros((nX, nY), dtype=DTYPE)
 
-    cdef int[:, :] descendantX_arr = descendantX
-    cdef int[:, :] descendantY_arr = descendantY
+    cdef DTYPE_t[:, :] descendantX_arr = descendantX
+    cdef DTYPE_t[:, :] descendantY_arr = descendantY
 
     cdef double past_error = 1e20
     cdef double error = 0.0
 
     cdef int Jplus1, Jplus1_ 
 
-    cdef Py_ssize_t xMin, yMin
-    cdef Py_ssize_t xNext, yNext
+    cdef DTYPE_t xMin, yMin
+    cdef DTYPE_t xNext, yNext
  
-    cdef Py_ssize_t k, x, y, uIdx
+    cdef DTYPE_t k, x, y, uIdx
 
     cdef np.ndarray trajs = np.zeros((max_horizon + 1, 2), dtype=DTYPE)    
 
